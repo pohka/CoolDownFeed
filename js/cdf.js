dd6ca23aafdd5f3b132190a8ace21a6ccdcd297c
geff <zomg21@hotmail.com>
2017-11-26 00:43:58 +0000

    simple javascript navgation

--- js/cdf.js
+++ js/cdf.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+
+  $(".nav-item").click(function(){
+      switch($(this).attr("id"))
+      {
+        case "filter-discover":   window.open("/post.html", "_self"); break;
+        case "filter-popular":    window.open("/index.html", "_self"); break;
+        case "filter-home" :      window.open("/index.html", "_self"); break;
+      }
+  });
+
+  $(".footer-filter").click(function(){
+      switch($(this).attr("id"))
+      {
+        case "footer-about" :     window.open("/index.html", "_self"); break;
+        case "footer-discover":   window.open("/index.html", "_self"); break;
+      }
+  });
+});
+
+
 function login(){
   console.log("loggin");
 
